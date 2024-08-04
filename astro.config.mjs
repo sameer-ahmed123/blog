b31355c83ba985e566edb94d0edfdb01cb3bf497
sameer <sameerkhanjan123@gmail.com>
2024-08-04 22:08:37 +0500

    astro blog

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig, passthroughImageService } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  image: {
+    service: passthroughImageService(),
+  },
+  integrations: [tailwind(), mdx()]
+});
