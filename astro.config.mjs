f4a6cb9ba38ddcd0c2dacfc0f69f350a86f62b16
sameer <sameerkhanjan123@gmail.com>
2024-08-05 02:46:00 +0500

    ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import netlify from "@astrojs/netlify";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
   },
   integrations: [tailwind(), mdx()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel()
 });
